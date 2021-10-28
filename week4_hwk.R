1ef46829f7bc03f1fe3f0d7d8c9bd8b399ac9587
TimMeng19 <77499602+TimMeng19@users.noreply.github.com>
2021-10-28 22:57:11 +0100

    cleaned data

--- week4_hwk.R
+++ week4_hwk.R
@@ -6,7 +6,7 @@ library(fs)
 library(ggplot2) 
 library(plotly) 
 library(dplyr) 
-library(janitor)z 
+library(janitor) 
 library(maptools) 
 library(RColorBrewer) 
 library(classInt) 
@@ -16,7 +16,7 @@ library(tmap)
 library(tmaptools) 
 library(rgdal) 
 library(geojsonio) 
-
+library(dplyr)
 
 # load files --------------------------------------------------------------
 
@@ -31,5 +31,75 @@ world_country <- st_read(here::here("World_Countries_(Generalized).geojson"))
 plot(world_country)
 
 
+# add a new column in GII -------------------------------------------------
+
+
+GII_1 <- GII %>%
+  clean_names()
+
+#Check the current type status
+Datatypelist <- GII_1 %>% 
+  summarise_all(class) %>%
+  pivot_longer(everything(), 
+               names_to="All_variables", 
+               values_to="Variable_class")
+
+
+# replace all the '..' to NA in GII ---------------------------------------
+#naniar package: https://cran.r-project.org/web/packages/naniar/vignettes/replace-with-na.html
+
+library(naniar)
+GII_2 <- GII_1 %>% 
+  replace_with_na_all(condition = ~.x == '..')
+
+Datatypelist_2 <- GII_2 %>% 
+  summarise_all(class) %>%
+  pivot_longer(everything(), 
+               names_to="All_variables", 
+               values_to="Variable_class")
+
+
+# Change GII from Char to Num ---------------------------------------------
+
+
+GII_3 <- GII_2 %>% 
+  mutate(year_1995 = as.numeric(x1995),
+         year_2000 = as.numeric(x2000),
+         year_2005 = as.numeric(x2005),
+         year_2010 = as.numeric(x2010),
+         year_2011 = as.numeric(x2011),
+         year_2012 = as.numeric(x2012),
+         year_2013 = as.numeric(x2013),
+         year_2014 = as.numeric(x2014),
+         year_2015 = as.numeric(x2015),
+         year_2016 = as.numeric(x2016),
+         year_2017 = as.numeric(x2017),
+         year_2018 = as.numeric(x2018),
+         year_2019 = as.numeric(x2019),
+         )
+
+Datatypelist_3 <- GII_3 %>% 
+  summarise_all(class) %>%
+  pivot_longer(everything(), 
+               names_to="All_variables", 
+               values_to="Variable_class")
+
+
+# extract numeric columns -------------------------------------------------
+
+
+GII_num <-GII_3 %>%
+  dplyr::select(c(1,2,16,17,18,19,20,21,22,23,24,25,26,27,28))
+
+
+# calculate average GII in the most recent 3 years ------------------------
+
+
+GII_num <- GII_num %>% 
+  #new column with average of male and female life expectancy
+  mutate(average_GII_3_years = (`year_2019` + `year_2018` + `year_2017`)/3)
+
+
+qtm(world_country)
 
 
